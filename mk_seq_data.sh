1a80167a313787dc4fe4c3c458e2dcc7539453f5
mutsuki <mutsuki@momo86.net>
2018-02-25 11:53:04 +0900

    fix mat size span

--- mk_seq_data.sh
+++ mk_seq_data.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-for i in `seq 10 10 500`;do
+for i in `seq 100 100 3000`;do
 	b_time=$(./batched-k $i | grep 計算時間 | sed -e 's/計算時間 : //' | sed -e 's/\s\[ms\]//')
 	s_time=$(./seq-k $i | grep 計算時間 | sed -e 's/計算時間 : //' | sed -e 's/\s\[ms\]//')
 	echo "scale=3; $s_time / $b_time" | bc
