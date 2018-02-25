4d16b36daf9493b9710ded35351ad8e31d53edb0
mutsuki <mutsuki@momo86.net>
2018-02-25 09:43:31 +0900

    fix graph aspect ratio :( :( :( :( :( :(

--- mk_graph.py
+++ mk_graph.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 
 df = pd.read_csv("log-k", encoding="UTF-8")
-plt.figure(figsize=(4, 14))
+#plt.figure(figsize=(4, 4))
 #plt.rcParams['font.family'] = 'Ricty'
 #plt.rcParams['font.family'] = 'Meiryo'
 plt.rcParams["font.size"] = 22
