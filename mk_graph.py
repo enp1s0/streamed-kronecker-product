54e1b0e3438bb7cc9989a6037300c5b3839299f1
mutsuki <mutsuki@momo86.net>
2018-02-25 09:31:09 +0900

    add mk_graph.py

---
+++ mk_graph.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+df = pd.read_csv("log-k", encoding="UTF-8")
+plt.figure(figsize=(6, 7.3))
+#plt.rcParams['font.family'] = 'Ricty'
+#plt.rcParams['font.family'] = 'Meiryo'
+plt.rcParams["font.size"] = 22
+plt.rcParams["xtick.labelsize"] = 12
+plt.rcParams["ytick.labelsize"] = 15
+plt.rcParams["legend.fontsize"] = 18
+plt.grid()
+
+print(df)
+
+
+colorlist = ["#2ca9e1","#e95295","#aacf53","#f8b500"]
+
+data = df["r"]
+m_size=range(0,29)
+
+plt.axhline(y=0)
+plt.ylim(ymax=2.0)
+plt.ylim(ymin=1.0)
+plt.xlim(xmin=0)
+plt.xlim(xmax=50)
+
+
+plt.xlabel("matrix size N x N (N)")
+plt.ylabel("Speed up")
+
+
+plt.plot(m_size,data,"r",color=colorlist.pop(),markersize=2)
+plt.legend()
+
+plt.savefig("speedup.pdf", bbox_inches="tight")
